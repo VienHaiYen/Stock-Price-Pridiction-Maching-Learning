99d8eb84ce30ac4023dd714d44346b188b539e6e
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2024-06-29 18:42:13 +0700

    get data to csv file

---
+++ stock_data.py
@@ -0,0 +1,27 @@
+import yfinance as yf
+from datetime import date, timedelta
+
+
+def getStockData(stock, start_date, end_date):
+  # get data
+  yf_clone = yf
+  data = yf_clone.download(stock, start_date, end_date)
+  return data
+
+def getStockDataToNow(stock, days):
+  end_date = date.today()
+  start_date = end_date - timedelta(days)
+  data = getStockData(stock, start_date, end_date)
+
+  return data
+
+
+def getAllDataToCSV():
+  coins = ["BTC-USD", "ETH-USD",
+        #  "BNB-USD", "ADA-USD", "XRP-USD",
+        #  "SOL-USD", "DOT-USD", "DOGE-USD", "SHIB-USD", "LTC-USD"
+         ]
+
+  for coin in coins:
+    data = getStockDataToNow(coin, 5*365)
+    data.to_csv(f'./data/{coin}.csv')
